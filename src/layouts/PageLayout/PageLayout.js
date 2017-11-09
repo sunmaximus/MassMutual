4c61025dda19b56c8b63bebf8380352246aaf238
Son Nguyen <sonnguyen@Sons-MBP.fios-router.home>
2017-11-09 00:34:57 -0500

    Finishing up the css

--- src/layouts/PageLayout/PageLayout.js
+++ src/layouts/PageLayout/PageLayout.js
@@ -5,14 +5,32 @@ import {
   Container,
 } from 'semantic-ui-react'
 
+import './PageLayout.scss'
+
 export const PageLayout = ({ children }) => (
   <Container>
-    <h1>React Redux Starter Kit</h1>
-    <IndexLink to='/'>Home</IndexLink>
-    {' · '}
-    <Link to='/counter'>Counter</Link>
-    {' . '}
-    <Link to='/test'>test</Link>
+    <Container>
+      <div className='black-box-container-top'>
+        <div style={{
+          display: 'flex',
+          alignItems: 'center',
+          justifyContent: 'center',
+          height: '100%',
+          color: 'white',
+        }}><h1>THE TIMES</h1></div>
+      </div>
+
+      <div style={{ height: '52px', width: '100%', 
+         boxShadow : '0 1px 2px 0 rgba(34,36,38,.15)',
+        border: '1px solid rgba(34,36,38,.15)', marginBottom: '60px' }}>
+        <IndexLink to='/'>Home</IndexLink>
+        {' * '}
+        <Link to='/counter'>Counter</Link>
+        {' * '}
+        <Link to='/test'>test</Link>
+      </div>
+    </Container>
+
     {children}
   </Container>
 )
